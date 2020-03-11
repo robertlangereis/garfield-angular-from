bb5a37c215bc6996d7ffa709702b36fbbdb269bc
robert <robert.langereis@gmail.com>
2020-03-11 12:04:29 +0100

    checkboxes implemented with required check

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,24 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { FormService } from '../form.service';
+import { Component, OnInit } from "@angular/core";
+import { FormService } from "../form.service";
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  selector: "app-form",
+  templateUrl: "./form.component.html",
+  styleUrls: ["./form.component.scss"]
 })
 export class FormComponent implements OnInit {
-
-  phoneNumber = '';
-  name = '';
-  email = '';
+  phoneNumber = "";
+  name = "";
+  email = "";
   formService: FormService;
-
+  nameChecked = false;
+  emailChecked = false;
+  phoneChecked = false;
+  formValid = false
 
   constructor(formService: FormService) {
     this.formService = formService;
   }
 
   ngOnInit() {
+    console.log(this.emailChecked);
+    console.log(this.phoneChecked);
+  }
+
+  checkboxCheck(input) {
+    console.log(input.checked, "event");
+    if (input.name === "name") this.nameChecked = input.checked;
+    if (input.name === "email") this.emailChecked = input.checked;
+    if (input.name === "phone") this.phoneChecked = input.checked;
   }
 
 
@@ -27,7 +38,10 @@ export class FormComponent implements OnInit {
       return;
     }
     console.log(submittedForm);
-    this.formService.addGarfieldFan(submittedForm.value.name, submittedForm.value.phone, submittedForm.value.email);
+    this.formService.addGarfieldFan(
+      submittedForm.value.name,
+      submittedForm.value.phone,
+      submittedForm.value.email
+    );
   }
-
 }
